129ac2919d734319d866f32ce5914f5d1b62f823
Yuanda Liu <72162739+yliu-hashed@users.noreply.github.com>
2025-05-12 22:25:16 -0400

    Track yosys v0.53

--- Docker/yosys/build-yosys.bash
+++ Docker/yosys/build-yosys.bash
@@ -16,7 +16,7 @@ rm -rf /var/lib/apt/lists
 git clone https://github.com/YosysHQ/yosys repo
 
 cd repo
-git checkout v0.52
+git checkout v0.53
 git submodule update --init
 
 cp /Docker/yosys-make.conf Makefile.conf

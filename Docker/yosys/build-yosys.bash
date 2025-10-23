be16fb1697b049c8dd86593815f767092d7c4bcd
Yuanda Liu <72162739+yliu-hashed@users.noreply.github.com>
2025-10-23 00:38:50 -0400

    Track Yosys v0.58

--- Docker/yosys/build-yosys.bash
+++ Docker/yosys/build-yosys.bash
@@ -6,7 +6,7 @@ apt-get update -qq
 apt-get -y install --no-install-recommends \
   build-essential clang lld ca-certificates curl \
   libffi-dev libreadline-dev zlib1g-dev python3 \
-  bison flex gawk git iverilog pkg-config make
+  bison flex libfl-dev gawk git iverilog pkg-config make
 apt-get autoclean
 apt-get clean
 apt-get -y autoremove
@@ -16,7 +16,7 @@ rm -rf /var/lib/apt/lists
 git clone https://github.com/YosysHQ/yosys repo
 
 cd repo
-git checkout v0.53
+git checkout v0.58
 git submodule update --init
 
 cp /Docker/yosys-make.conf Makefile.conf

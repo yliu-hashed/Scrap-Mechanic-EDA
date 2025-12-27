39d7ef33c334987e1d21635b5ac2a615cf06f799
Yuanda Liu <72162739+yliu-hashed@users.noreply.github.com>
2025-12-26 19:28:22 -0500

    Solidify subprocess version

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.5.0"),
-        .package(url: "https://github.com/swiftlang/swift-subprocess.git", branch: "main"),
+        .package(url: "https://github.com/swiftlang/swift-subprocess.git", exact: "0.2.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
